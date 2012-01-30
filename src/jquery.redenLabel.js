f174a0f0247ade6a5593fd1340b8a51ab393be8c
Gergő Németh <reden87@reden87s-MacBook-Pro.local>
2012-01-30 12:40:53 +0100

    minimális frissítés, semmi újdonság

--- src/jquery.redenLabel.js
+++ src/jquery.redenLabel.js
@@ -38,8 +38,9 @@
 			$(this).parent('p').css( 'position', 'relative' );
 			$(this).css({
 				opacity: opacity, // ha eleve van valami a mezőben, akkor alapból láthatatlan a label
-				display: display,
-				cursor: 'text' // css cursor beállítás
+				display: display, // ha eleve van valami a mezőben, akkor ne zavarjon be ha egérrel ki akarom jelölni a szöveget
+				cursor: 'text', // css cursor beállítás
+				position: 'absolute'
 			});
 			
 			
@@ -50,12 +51,14 @@
 				}
 			});
 			
+			
 			// ha kimegy belőle a focus, és üres, akkor sötétítés
 			$('input[id=' + $(this).attr('for') + '], textarea[id=' + $(this).attr('for') + ']').live( 'focusout', function() {
 				if( empty($(this).val()) ) {
 					set_opacity( $('label[for=' + $(this).attr('id') + ']'), 1 );
 				}
 			});
+			
 			
 			// ha lenyomunk egy billentyűt, és üres akkor közepesen halványra (akár sötét volt előtte, akár láthatatlan), különben láthatatlanítás
 			$('input[id=' + $(this).attr('for') + '], textarea[id=' + $(this).attr('for') + ']').live( 'keydown', function(e) {
@@ -69,18 +72,17 @@
 					key != 17 && // ctrl
 					key != 18 && // alt
 					key != 20 && // caps lock
-					key != 27 && // escape
 					key != 37 && // left arrow
 					key != 38 && // up arrow
 					key != 39 && // right arrow
 					key != 40 && // down arrow
-					key != 46 && // delete
 					key != 91 && // left window key
 					key != 92 && // right window key
 					key != 93 // right cmd key
 					) )
 					set_opacity( $('label[for=' + $(this).attr('id') + ']'), 0 );
 			});
+			
 			
 			// ha lenyomunk egy billentyűt, és üres akkor közepesen halványra (akár sötét volt előtte, akár láthatatlan), különben láthatatlanítás
 			$('input[id=' + $(this).attr('for') + '], textarea[id=' + $(this).attr('for') + ']').live( 'keyup', function() {
@@ -92,9 +94,17 @@
 				}
 			});
 			
+			
+			// paste event
+			$('input[id=' + $(this).attr('for') + '], textarea[id=' + $(this).attr('for') + ']').live( 'paste', function() {
+				// mivel üres sztringet nem tudunk paste-elni, ezért feltételezzük hogy szöveg került a mezőbe, és láthatatlanná tesszük a labelt
+				set_opacity( $('label[for=' + $(this).attr('id') + ']'), 0 );
+			});
+			
 			// lementjük az eredeti értékét a mezőknek, hogy reset gomb nyomásakor tudjunk ellenőrízni, hogy kell-e mutatni az input mezőt vagy sem
 			var base = $('input[id=' + $(this).attr('for') + '], textarea[id=' + $(this).attr('for') + ']');
 			var base_val = $('input[id=' + $(this).attr('for') + '], textarea[id=' + $(this).attr('for') + ']').val();
+			
 			
 			// ha reset gombra kattintunk, akkor mindet sötétítjük
 			$('input[type=reset]').live( 'click', function() {
@@ -126,12 +136,14 @@
 			});
 		}
 		
+		
 		// ellenőrzi, hogy egy string üres-e
 		function empty( val ) {
 			if( val == '' || val == null || val == undefined )
 				return true;
 			return false;
 		}
+		
 		
 	};
 })( jQuery );
